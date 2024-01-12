bf69f6ba94f33fdec67c06f05384d66a83789e31
Enrico <en.cozzolino93@gmail.com>
2024-01-12 18:05:36 +0100

    Style file, list, addItem and removeItem functions added

--- javascript.js
+++ javascript.js
@@ -1,4 +1,24 @@
 var app = angular.module("shopping-list", [])
 app.controller("ctr", function($scope){
-    
+    $scope.list = []
+    $scope.errMess = ""
+    $scope.addItem = function(){
+        $scope.errMess = ""
+        $scope.item = angular.uppercase($scope.item)
+        if(!$scope.item){
+            $scope.errMess = 'ERROR! YOU DID NOT ADD ANYMORE.'
+            return;
+        }
+        if($scope.list.indexOf($scope.item) === -1){
+            $scope.list.unshift($scope.item)
+            $scope.item = ""
+        }else{
+            $scope.errMess = 'ERROR! YOU HAVE ALREADY ADDED: '+$scope.item+'.'
+            $scope.item = ""
+        }
+    }
+    $scope.removeItem = function(x){
+        $scope.errMess = ""
+        $scope.list.splice(x, 1)
+    }
 })
